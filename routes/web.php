f9d22c9267a49bf602689d3c3a836caed246f1b7
vagrant <vagrant@homestead>
2018-11-14 14:35:55 +0000

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/','StaticPagesController@hoome');
+Route::get('/help','StaticPagesController@help');
+Route::get('/about','StaticPagesController@about');
